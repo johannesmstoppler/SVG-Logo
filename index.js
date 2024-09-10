ebb72f4f932d3c917a2206d1f447f3f021f69958
Johannes Stoppler <johannesstoppler@Johanness-MBP.attlocal.net>
2024-09-10 15:14:34 -0700

    svg html update

--- index.js
+++ index.js
@@ -27,13 +27,37 @@ inquirer
         /* Pass your questions in here */
     ])
     .then((answers) => {
-        const content = `<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="300" height="200">
+        let content
+        if (answers.shape === "square") {
+            content = `<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="300" height="200">
+
+<rect x="30" y="30" width="300" height="300" fill="${answers.shapecolor}"/>
+
+  <text x="160" y="125" font-size="60" text-anchor="middle" fill="${answers.textcolor}">${answers.text}</text>
+
+</svg>
+`
+        }
+        if (answers.shape === "circle") {
+            content = `<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="300" height="200">
 
             <circle cx = "150" cy = "100" r = "80" fill = "${answers.shapecolor}" />
 
                 <text x="150" y="125" font-size="60" text-anchor="middle" fill="${answers.textcolor}">${answers.text}</text>
 
 </svg > `;
+
+        }
+
+        if (answers.shape === "triangle") {
+            content = `<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="600" height="500">
+
+<polygon points="250,60 100,400 400,400" x="200" y="200" fill="${answers.shapecolor}"/>
+
+  <text x="200" y="300" font-size="60" text-anchor="center" fill="${answers.textcolor}">${answers.text}</text>
+
+</svg>`
+        }
         fs.writeFile('./examples/shapes.svg', content, err => {
             if (err) {
                 console.error(err);
